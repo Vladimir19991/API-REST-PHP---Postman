f9709fcbab1dea1068614a6b3759a01fea3dac3a
Vladimir19991 <paul_189@hotmail.es>
2022-12-12 09:29:05 -0600

    corecciones
    Corecciones que estaban malas

--- connection/connection.php
+++ connection/connection.php
@@ -2,7 +2,7 @@
 
     class Connection extends  Mysqli {
         function __construct(){
-            parent::__construct('localhost', 'root', '', 'Apirest_video2');
+            parent::__construct('localhost', 'root', '', 'apirest_video2');
             $this->set_charset('utf8');
             $this->connect_error == NULL ? 'Conexion exito a la BD': die ('Error al conectar a la BD');
         }//end de la fuction
--- models/cliente.php
+++ models/cliente.php
@@ -1,95 +1,83 @@
 <?php
-    require_once "connection/connection.php";
+    require_once "connection/Connection.php";
 
     class Cliente {
-        public static function getAll(){
+
+        public static function getAll() {
             $db = new Connection();
-            $query = "SELECT * FROM clientes";
+            $query = "SELECT *FROM clientes";
             $resultado = $db->query($query);
             $datos = [];
-            if ($resultado->num_rows){
-                while ($row = $resultado->fetch_assoc()){
-                $datos[] = [
-                    'id' => $row['id'],
-                    'nombre' => $row['nombre'],
-                    'ap' => $row['ap'],
-                    'am' => $row['ap'],
-                    'fn' => $row['fn'],
-                    'genero' => $row['genero'],
+            if($resultado->num_rows) {
+                while($row = $resultado->fetch_assoc()) {
+                    $datos[] = [
+                        'id' => $row['id'],
+                        'nombre' => $row['nombre'],
+                        'ap' => $row['ap'],
+                        'am' => $row['am'],
+                        'fn' => $row['fn'],
+                        'genero' => $row['genero']
                     ];
                 }//end while
                 return $datos;
             }//end if
             return $datos;
-        }//end function getAll()
+        }//end getAll
 
-
-
-        public static function getWhere($id_clientes){
+        public static function getWhere($id_cliente) {
             $db = new Connection();
-            $query = "SELECT * FROM clientes WHERE id=$id_clientes";
+            $query = "SELECT *FROM clientes WHERE id=$id_cliente";
             $resultado = $db->query($query);
             $datos = [];
-            if ($resultado->num_rows){
-                while ($row = $resultado->fetch_assoc()){
-                $datos[] = [
-                    'id' => $row['id'],
-                    'nombre' => $row['nombre'],
-                    'ap' => $row['ap'],
-                    'am' => $row['ap'],
-                    'fn' => $row['fn'],
-                    'genero' => $row['genero'],
+            if($resultado->num_rows) {
+                while($row = $resultado->fetch_assoc()) {
+                    $datos[] = [
+                        'id' => $row['id'],
+                        'nombre' => $row['nombre'],
+                        'ap' => $row['ap'],
+                        'am' => $row['am'],
+                        'fn' => $row['fn'],
+                        'genero' => $row['genero']
                     ];
                 }//end while
                 return $datos;
             }//end if
             return $datos;
-        }//end function getWhere()
+        }//end getWhere
 
-
-
-        public static function insert($nombre, $ap, $am, $fn, $genero){
+        public static function insert($nombre, $ap, $am, $fn, $genero) {
             $db = new Connection();
-            $query = "INSERT INTO clientes (nombre,ap,am,fn,genero)
-            VALUES ('".$nombre."', '".$ap."', '".$am."', '".$fn."', '".$genero."')";
+            $query = "INSERT INTO clientes (nombre, ap, am, fn, genero)
+            VALUES('".$nombre."', '".$ap."', '".$am."', '".$fn."', '".$genero."')";
             $db->query($query);
-            if ($db->affected_rows){
-                return true;
-            }
-            return false;
-
+            if($db->affected_rows) {
+                return TRUE;
+            }//end if
+            return FALSE;
         }//end insert
 
-
-        public static function update($id_cliente,$nombre, $ap, $am, $fn, $genero){
+        public static function update($id_cliente, $nombre, $ap, $am, $fn, $genero) {
             $db = new Connection();
             $query = "UPDATE clientes SET
             nombre='".$nombre."', ap='".$ap."', am='".$am."', fn='".$fn."', genero='".$genero."' 
             WHERE id=$id_cliente";
             $db->query($query);
-            if ($db->affected_rows){
-                return true;
-            }
-            return false;
-
-        }//end insert
-
-
-
-        public static function delete ($id_cliente){
-            $db = new Connection();
-            $query = "DELETE FROM clientes WHERE id = $id_cliente";
-            $db->query($query);
-            if ($db->affected_rows){
-                return true;
+            if($db->affected_rows) {
+                return TRUE;
             }//end if
-            return false;
+            return FALSE;
+        }//end update
 
+        public static function delete($id_cliente) {
+            $db = new Connection();
+            $query = "DELETE FROM clientes WHERE id=$id_cliente";
+            $db->query($query);
+            if($db->affected_rows) {
+                return TRUE;
+            }//end if
+            return FALSE;
         }//end delete
 
-
-
     }//end class Cliente
-
 
 ?>
